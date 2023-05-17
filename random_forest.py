b13e40926bfd1f17ba339ef5055b89e0a3d5afb4
Jonathan Kjellen <jonathan.kjellen.3657@student.uu.se>
2023-05-17 17:20:47 +0200

    added random forest and app skeleton

---
+++ random_forest.py
@@ -0,0 +1,33 @@
+from joblib import dump, load
+from numpy import loadtxt
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.metrics import r2_score
+
+# load the dataset
+dataset = pd.read_csv('PreprocessData.csv',index_col=0)
+
+X = dataset.drop(['stars'] , axis =1)
+y = dataset.stars
+
+s = StandardScaler()
+X = s.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
+clf = RandomForestClassifier(n_jobs=-1, n_estimators=10, verbose=1, random_state=42)
+clf.fit(X_train, y_train)
+
+test_score = r2_score(y_test, clf.predict(X_test))
+
+
+print("test set performance: ", test_score)
+
+# Saving model to file
+dump(clf, 'model.joblib') 
+
+
+
+
