ebff4a1ed487a867f73a2c24a910ff0e4e3f24cf
Jonathan Kjellen <jonathan.kjellen.3657@student.uu.se>
2023-05-22 09:27:20 +0200

    added language feature

--- convert_json_to_csv.py
+++ convert_json_to_csv.py
@@ -17,8 +17,35 @@ data = {'size': [],
         'has_pages':[],
         'has_issues':[],
         'has_projects':[],
-        'num_topics':[]
+        'num_topics':[],
+        'javascript':[],
+        'typescript':[],
+        'python':[],
+        'other_language':[]
+
         }
+def check_language(language):
+    if language == 'JavaScript':
+        data['javascript'].append(1)  
+        data['typescript'].append(0)
+        data['python'].append(0) 
+        data['other_language'].append(0)
+    elif language == 'TypeScript':  
+        data['typescript'].append(1) 
+        data['javascript'].append(0) 
+        data['python'].append(0) 
+        data['other_language'].append(0)
+    elif language == 'Python':
+        data['python'].append(1) 
+        data['javascript'].append(0) 
+        data['typescript'].append(0)
+        data['other_language'].append(0)
+    else:
+        data['other_language'].append(1)
+        data['javascript'].append(0)  
+        data['typescript'].append(0)
+        data['python'].append(0) 
+
 
 for item in sh_json['items']:
     data['size'].append(item['size'])
@@ -30,6 +57,7 @@ for item in sh_json['items']:
     data['has_issues'].append(item['has_issues'])
     data['has_projects'].append(item['has_projects'])
     data['num_topics'].append(len(item['topics']))
+    check_language(item['language'])
 
 
 
